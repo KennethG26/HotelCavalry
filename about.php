baf862424ff7bdacdf6f771d5fd95fff83d92c06
KennethG26 <109411316+KennethG26@users.noreply.github.com>
2022-07-18 19:13:09 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>HOTEL CAVARLY</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Resort Inn Responsive , Smartphone Compatible web template , Samsung, LG, Sony Ericsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen">
+<link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
+<link rel="stylesheet" href="css/jquery-ui.css" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/modernizr-2.6.2.min.js"></script>
+<!--fonts-->
+<link href="//fonts.googleapis.com/css?family=Oswald:300,400,700" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Federo" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+<!--//fonts-->
+</head>
+<style>
+	
+    .menu__item a::after{
+	content: "";
+	display: block;
+	transform: scale(0,1);
+	width: 100%;
+	height: 4px;
+    background: #ffce14;
+	transition: transform 200ms ease;
+}
+.menu__item a:hover::after{
+	transform: scale(1,1);
+	cursor: pointer;
+}
+
+.agileits-banner-info p {
+    color: #ffce14;
+    font-size: 1.8em;
+    line-height: 1.8em;
+    letter-spacing: 11px;
+    margin: 1em auto;
+    font-weight: bolder;
+    text-transform: uppercase;
+}
+p.about-para-w3ls {
+color: black;
+}
+.callbacks_tabs li {
+    display: none;
+    margin: 30px 7px;
+}
+</style>
+
+<body>
+<!-- header -->
+<div class="banner-top">
+			<div class="social-bnr-agileits">
+				<ul class="social-icons3">
+								<li><a href="https://www.facebook.com/" class="fa fa-facebook icon-border facebook"> </a></li>
+								<li><a href="https://twitter.com/" class="fa fa-twitter icon-border twitter"> </a></li>
+								<li><a href="https://plus.google.com/u/0/" class="fa fa-google-plus icon-border googleplus"> </a></li> 
+							</ul>
+			</div>
+			<div class="contact-bnr-w3-agile">
+				<ul>
+					<li><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:info@example.com">hotelcavarly@gmail.com</a></li>
+					<li><i class="fa fa-phone" aria-hidden="true"></i>+63 (09)357-228-506</li>	
+					<li class="s-bar">
+						<div class="search">
+							<input class="search_box" type="checkbox" id="search_box">
+							<label class="icon-search" for="search_box"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></label>
+							<div class="search_form">
+								<form action="#" method="post">
+									<input type="search" name="Search" placeholder=" " required=" " />
+									<input type="submit" value="Search">
+								</form>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	<div class="w3_navigation">
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<div class="navbar-header navbar-left">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<h1><a class="navbar-brand" href="index.php">Hotel <span>Cavarly</span><p class="logo_w3l_agile_caption">Your Dreamy Hotel</p></a></h1>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+					<nav class="menu menu--iris">
+						<ul class="nav navbar-nav menu__list">
+							<li class="menu__item menu__item--current"><a href="index.php">Home</a></li>
+							<li class="menu__item"><a href="#about" >About</a></li>
+							<li class="menu__item"><a href="gallery.php" >Gallery</a></li>
+							<li class="menu__item"><a href="#rooms">Rooms</a></li>
+							<li class="menu__item"><a href="#contact">Contact Us</a></li>
+						</ul>
+					</nav>
+				</div>
+			</nav>
+
+		</div>
+	</div>
+<!-- //header -->
+
+<!-- /about -->
+<div class="about-wthree" id="about">
+		  <div class="container">
+				   <div class="ab-w3l-spa">
+                            <h3 class="title-w3-agileits title-black-wthree">About Our CAVARLY</h3> 
+						   <p class="about-para-w3ls">The "Cavalry" name is inspired from the windbreaker manhwa. The Hotel Calvary is a celebrated balance of nostalgia and modern style, capturing its original elegance, luxury, and decadence. In the dramatic suites, hotel hardwood floors contrast with modern furnishings and amenities. The Hotel provides guests with the finest organic toiletries, heirloom recipes, and unrivaled privacy and tranquility.</p>
+						   <img src="images/about.jpg" class="img-responsive" alt="Hair Salon">
+										<div class="w3l-slider-img">
+											<img src="images/a1.jpg" class="img-responsive" alt="Hair Salon">
+										</div>
+                                       <div class="w3ls-info-about">
+										    <h4>You'll love all the amenities we offer!</h4>
+										</div>
+		          </div>
+		   	<div class="clearfix"> </div>
+    </div>
+</div>
+ 	<!-- //about -->
+
+     <div class="copy">
+		        <p>© 2022 All Rights Reserved <a href="index.php">CAVARLY</a> </p>
+		    </div>
+<!--/footer -->
+<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
+</body>
+</html>
